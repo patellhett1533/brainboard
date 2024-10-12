71645096936c9fbdfa6f1d22a2a5260fdbadc18e
het patel <hppatidar12d@gmail.com>
2024-10-12 21:13:01 +0530

    update shadcn setup

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,8 +1,30 @@
 'use client'
+import { Button } from '@/components/ui/button'
 import React, { useEffect, useRef } from 'react'
+
+interface Response {
+  expr: string
+  result: string
+  assign: string
+}
+
+interface GeneratedResult {
+  expression: string
+  answer: string
+}
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  const [isDrawing, setIsDrawing] = React.useState(false)
+  const [isDrawing, setIsDrawing] = React.useState<Boolean>(false)
+  const [reset, setReset] = React.useState<Boolean>(false)
+  const [result, setResult] = React.useState<GeneratedResult>()
+  const [dictOfVars, setDictOfVars] = React.useState<Record<string, string>>({})
+
+  useEffect(() => {
+    if (reset) {
+      resetCanvas()
+      setReset(false)
+    }
+  }, [reset])
 
   useEffect(() => {
     const canvas = canvasRef.current
@@ -48,15 +70,33 @@ export default function Home() {
     }
   }
 
+  const resetCanvas = () => {
+    const canvas = canvasRef.current
+    if (canvas) {
+      const ctx = canvas.getContext('2d')
+      if (ctx) ctx.clearRect(0, 0, canvas.width, canvas.height)
+    }
+  }
+
   return (
-    <canvas
-      ref={canvasRef}
-      id="canvas"
-      className="absolute top-0 left-0 w-full h-dvh"
-      onMouseDown={startDrawing}
-      onMouseOut={stopDrawing}
-      onMouseUp={stopDrawing}
-      onMouseMove={draw}
-    />
+    <>
+      <div className="w-full h-dvh flex justify-end items-end p-8">
+        <Button
+          onClick={() => resetCanvas()}
+          className="rounded-lg select-none"
+        >
+          Clear
+        </Button>
+      </div>
+      <canvas
+        ref={canvasRef}
+        id="canvas"
+        className="absolute top-0 left-0 w-full h-dvh"
+        onMouseDown={startDrawing}
+        onMouseOut={stopDrawing}
+        onMouseUp={stopDrawing}
+        onMouseMove={draw}
+      />
+    </>
   )
 }
